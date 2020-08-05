dc8e5c36b8a8e5c4c0c2085c463c6f6cdc7bca8e
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-05 17:59:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  count = 0 
+  
+  while count <= 5 do
+    puts string
+    count += 1
+  end
+end
+
+def loop_message_n_times(string, integer)
+  count = 0 
+  
+  while count <= integer do
+    puts string
+    count += 1
+  end
+end
+
+
+  
